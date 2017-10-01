872f1ef268c1c8fe242a110fbb9a0629ff03f7d7
Jailton <jottha_2006@hotmail.com>
2017-10-01 16:45:11 -0300

    Intergração com firebase

--- jdm/src/app/app.module.ts
+++ jdm/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {AngularFireModule} from 'angularFire2';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig =
+{   apiKey: "AIzaSyC_ILhKFZOvsCJY4oZ8_xTzhdxe7byO75I",
+    authDomain: "appjdm-lab-si.firebaseapp.com",
+    databaseURL: "https://appjdm-lab-si.firebaseio.com",
+    projectId: "appjdm-lab-si",
+    storageBucket: "appjdm-lab-si.appspot.com",
+    messagingSenderId: "308441218619"
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
