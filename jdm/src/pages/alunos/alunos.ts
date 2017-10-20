2b7a329439f3d87f924226774d3d9e9a81185ef6
Jailton <jottha_2006@hotmail.com>
2017-10-19 21:06:09 -0300

    modificações

--- jdm/src/pages/alunos/alunos.ts
+++ jdm/src/pages/alunos/alunos.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 import { FichaPage } from '../ficha/ficha';
-
-import { FirebaseListObservable } from 'angularfire2';
+import { Firebase } from '@ionic-native/firebase';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
 
 import { Aluno } from './../../models/aluno.models';
 import { AlunoProvider } from './../../providers/aluno/aluno';
@@ -15,11 +15,28 @@ import { AlunoProvider } from './../../providers/aluno/aluno';
 })
 export class AlunosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private firebase: Firebase,
+              public navCtrl: NavController,
+              public navParams: NavParams)
+  {   
+      
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AlunosPage');
+  }
+
+  getToken()
+  {
+    this.firebase.getToken()
+    .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
+    .catch(error => console.error('Error getting token', error));
+  }
+
+  onTokenRefresh()
+  {
+    this.firebase.onTokenRefresh()
+    .subscribe((token: string) => console.log(`Got a new token ${token}`));
   }
 
 }
